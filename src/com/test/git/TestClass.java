f28f71bad2f7144490eee8fab49836c7b6180313
Biju <bijusuma@gmail.com>
2014-06-23 13:20:20 -0400

    initial commit

---
+++ src/com/test/git/TestClass.java
@@ -0,0 +1,13 @@
+package com.test.git;
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Creating feature1 branch to merge into dev");
+	}
+
+}
---
+++ src/com/test/git/TestClass4.java
@@ -0,0 +1,14 @@
+package com.test.git;
+
+public class TestClass4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("dev branch");
+	}
+
+}
